92833a4e763f6b5d2d1f9f24298f3e2865222e08
Milind Ganjoo <milind.ganjoo@gmail.com>
2021-04-23 23:08:35 -0700

    Install Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,19 @@ const App: React.FC = () => {
   const [whiteToPlay, setWhiteToPlay] = useState(true)
 
   return (
-    <div>
+    <div className="p-3 w-96">
       <Chessground orientation={whiteToPlay ? "white" : "black"} />
-      <button onClick={() => setWhiteToPlay(!whiteToPlay)}>Flip</button>
+      <div className="flex flex-auto space-x-2">
+        <button
+          className="w-1/2 flex items-center justify-center rounded-md border border-gray-300"
+          onClick={() => setWhiteToPlay(!whiteToPlay)}
+        >
+          Flip
+        </button>
+        <button className="w-1/2 flex items-center justify-center rounded-md border border-gray-300">
+          Reset
+        </button>
+      </div>
     </div>
   )
 }
